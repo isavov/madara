18ba0fbe9f73ece03352a4b779c6f50af9e5825a
github-action-benchmark <github@users.noreply.github.com>
2024-03-07 11:06:42 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 642786e5a0a839833cd04312bfc6cb285a5a77c2

---
+++ dev/bench-failed/data.js
@@ -0,0 +1,54 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1709809602831,
+  "repoUrl": "https://github.com/isavov/madara",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "name": "Iliya Savov",
+            "username": "isavov",
+            "email": "isavov@users.noreply.github.com"
+          },
+          "committer": {
+            "name": "Iliya Savov",
+            "username": "isavov",
+            "email": "isavov@users.noreply.github.com"
+          },
+          "id": "642786e5a0a839833cd04312bfc6cb285a5a77c2",
+          "message": "feat: fail pipeline on failed requests\n\nSigned-off-by: Iliya Savov <isavov@users.noreply.github.com>",
+          "timestamp": "2024-03-07T11:05:41Z",
+          "url": "https://github.com/isavov/madara/commit/642786e5a0a839833cd04312bfc6cb285a5a77c2"
+        },
+        "date": 1709809602222,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Failed Transactions Verifications (Erc20 Transfers)",
+            "value": 0,
+            "unit": "",
+            "extra": "CPU Count: 8\nCPU Model: \nCPU Speed (MHz): 0\nTotal Memory: 16 GB\nPlatform: MacOS 14.3\nRelease: 23.3.0\nArchitecture: aarch64"
+          },
+          {
+            "name": "Failed Transaction Submissions (Erc20 Transfers)",
+            "value": 0,
+            "unit": "",
+            "extra": "CPU Count: 8\nCPU Model: \nCPU Speed (MHz): 0\nTotal Memory: 16 GB\nPlatform: MacOS 14.3\nRelease: 23.3.0\nArchitecture: aarch64"
+          },
+          {
+            "name": "Failed Transactions Verifications (Erc721 Mints)",
+            "value": 0,
+            "unit": "",
+            "extra": "CPU Count: 8\nCPU Model: \nCPU Speed (MHz): 0\nTotal Memory: 16 GB\nPlatform: MacOS 14.3\nRelease: 23.3.0\nArchitecture: aarch64"
+          },
+          {
+            "name": "Failed Transaction Submissions (Erc721 Mints)",
+            "value": 0,
+            "unit": "",
+            "extra": "CPU Count: 8\nCPU Model: \nCPU Speed (MHz): 0\nTotal Memory: 16 GB\nPlatform: MacOS 14.3\nRelease: 23.3.0\nArchitecture: aarch64"
+          }
+        ]
+      }
+    ]
+  }
+}
