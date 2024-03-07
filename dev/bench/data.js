3fb82490ad53a76cf940d18f4bfb18496f6c1eb0
github-action-benchmark <github@users.noreply.github.com>
2024-03-07 11:06:40 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for 642786e5a0a839833cd04312bfc6cb285a5a77c2

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1709809187052,
+  "lastUpdate": 1709809600984,
   "repoUrl": "https://github.com/isavov/madara",
   "entries": {
     "Benchmark": [
@@ -91,6 +91,76 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/isavov/madara/commit/459bd2153922ac10709d1bef9b27843f186907c7"
         },
         "date": 1709809182348,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Average TPS (Erc20 Transfers)",
+            "value": 65.83333333333333,
+            "unit": "transactions/second",
+            "extra": "CPU Count: 8\nCPU Model: \nCPU Speed (MHz): 0\nTotal Memory: 16 GB\nPlatform: MacOS 14.3\nRelease: 23.3.0\nArchitecture: aarch64"
+          },
+          {
+            "name": "Average Extrinsics per block (Erc20 Transfers)",
+            "value": 395,
+            "unit": "extrinsics/block",
+            "extra": "CPU Count: 8\nCPU Model: \nCPU Speed (MHz): 0\nTotal Memory: 16 GB\nPlatform: MacOS 14.3\nRelease: 23.3.0\nArchitecture: aarch64"
+          },
+          {
+            "name": "Average UOPS (Erc20 Transfers)",
+            "value": 65.83333333333333,
+            "unit": "operations/second",
+            "extra": "CPU Count: 8\nCPU Model: \nCPU Speed (MHz): 0\nTotal Memory: 16 GB\nPlatform: MacOS 14.3\nRelease: 23.3.0\nArchitecture: aarch64"
+          },
+          {
+            "name": "Average Steps Per Second (Erc20 Transfers)",
+            "value": 0,
+            "unit": "operations/second",
+            "extra": "CPU Count: 8\nCPU Model: \nCPU Speed (MHz): 0\nTotal Memory: 16 GB\nPlatform: MacOS 14.3\nRelease: 23.3.0\nArchitecture: aarch64"
+          },
+          {
+            "name": "Average TPS (Erc721 Mints)",
+            "value": 69.75,
+            "unit": "transactions/second",
+            "extra": "CPU Count: 8\nCPU Model: \nCPU Speed (MHz): 0\nTotal Memory: 16 GB\nPlatform: MacOS 14.3\nRelease: 23.3.0\nArchitecture: aarch64"
+          },
+          {
+            "name": "Average Extrinsics per block (Erc721 Mints)",
+            "value": 418.5,
+            "unit": "extrinsics/block",
+            "extra": "CPU Count: 8\nCPU Model: \nCPU Speed (MHz): 0\nTotal Memory: 16 GB\nPlatform: MacOS 14.3\nRelease: 23.3.0\nArchitecture: aarch64"
+          },
+          {
+            "name": "Average UOPS (Erc721 Mints)",
+            "value": 70.83333333333333,
+            "unit": "operations/second",
+            "extra": "CPU Count: 8\nCPU Model: \nCPU Speed (MHz): 0\nTotal Memory: 16 GB\nPlatform: MacOS 14.3\nRelease: 23.3.0\nArchitecture: aarch64"
+          },
+          {
+            "name": "Average Steps Per Second (Erc721 Mints)",
+            "value": 0,
+            "unit": "operations/second",
+            "extra": "CPU Count: 8\nCPU Model: \nCPU Speed (MHz): 0\nTotal Memory: 16 GB\nPlatform: MacOS 14.3\nRelease: 23.3.0\nArchitecture: aarch64"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Iliya Savov",
+            "username": "isavov",
+            "email": "isavov@users.noreply.github.com"
+          },
+          "committer": {
+            "name": "Iliya Savov",
+            "username": "isavov",
+            "email": "isavov@users.noreply.github.com"
+          },
+          "id": "642786e5a0a839833cd04312bfc6cb285a5a77c2",
+          "message": "feat: fail pipeline on failed requests\n\nSigned-off-by: Iliya Savov <isavov@users.noreply.github.com>",
+          "timestamp": "2024-03-07T11:05:41Z",
+          "url": "https://github.com/isavov/madara/commit/642786e5a0a839833cd04312bfc6cb285a5a77c2"
+        },
+        "date": 1709809592521,
         "tool": "customBiggerIsBetter",
         "benches": [
           {
