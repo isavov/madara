22f72b29103e7df1d680e825b2a0192aafb6ef78
github-action-benchmark <github@users.noreply.github.com>
2024-03-07 11:08:18 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 642786e5a0a839833cd04312bfc6cb285a5a77c2

--- dev/bench-failed/data.js
+++ dev/bench-failed/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1709809602831,
+  "lastUpdate": 1709809698921,
   "repoUrl": "https://github.com/isavov/madara",
   "entries": {
     "Benchmark": [
@@ -21,6 +21,52 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/isavov/madara/commit/642786e5a0a839833cd04312bfc6cb285a5a77c2"
         },
         "date": 1709809602222,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Failed Transactions Verifications (Erc20 Transfers)",
+            "value": 0,
+            "unit": "",
+            "extra": "CPU Count: 8\nCPU Model: \nCPU Speed (MHz): 0\nTotal Memory: 16 GB\nPlatform: MacOS 14.3\nRelease: 23.3.0\nArchitecture: aarch64"
+          },
+          {
+            "name": "Failed Transaction Submissions (Erc20 Transfers)",
+            "value": 0,
+            "unit": "",
+            "extra": "CPU Count: 8\nCPU Model: \nCPU Speed (MHz): 0\nTotal Memory: 16 GB\nPlatform: MacOS 14.3\nRelease: 23.3.0\nArchitecture: aarch64"
+          },
+          {
+            "name": "Failed Transactions Verifications (Erc721 Mints)",
+            "value": 0,
+            "unit": "",
+            "extra": "CPU Count: 8\nCPU Model: \nCPU Speed (MHz): 0\nTotal Memory: 16 GB\nPlatform: MacOS 14.3\nRelease: 23.3.0\nArchitecture: aarch64"
+          },
+          {
+            "name": "Failed Transaction Submissions (Erc721 Mints)",
+            "value": 0,
+            "unit": "",
+            "extra": "CPU Count: 8\nCPU Model: \nCPU Speed (MHz): 0\nTotal Memory: 16 GB\nPlatform: MacOS 14.3\nRelease: 23.3.0\nArchitecture: aarch64"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Iliya Savov",
+            "username": "isavov",
+            "email": "isavov@users.noreply.github.com"
+          },
+          "committer": {
+            "name": "Iliya Savov",
+            "username": "isavov",
+            "email": "isavov@users.noreply.github.com"
+          },
+          "id": "642786e5a0a839833cd04312bfc6cb285a5a77c2",
+          "message": "feat: fail pipeline on failed requests\n\nSigned-off-by: Iliya Savov <isavov@users.noreply.github.com>",
+          "timestamp": "2024-03-07T11:05:41Z",
+          "url": "https://github.com/isavov/madara/commit/642786e5a0a839833cd04312bfc6cb285a5a77c2"
+        },
+        "date": 1709809698667,
         "tool": "customSmallerIsBetter",
         "benches": [
           {
