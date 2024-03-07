98d805604f09eb99b92c7cf5be04ffd477d4dd85
github-action-benchmark <github@users.noreply.github.com>
2024-03-07 10:56:04 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for 98d433d4aac1a133d91237af68fc2365bba82984

---
+++ dev/bench/data.js
@@ -0,0 +1,78 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1709808964149,
+  "repoUrl": "https://github.com/isavov/madara",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "name": "Iliya Savov",
+            "username": "isavov",
+            "email": "isavov@users.noreply.github.com"
+          },
+          "committer": {
+            "name": "Iliya Savov",
+            "username": "isavov",
+            "email": "isavov@users.noreply.github.com"
+          },
+          "id": "98d433d4aac1a133d91237af68fc2365bba82984",
+          "message": "x: remove\n\nSigned-off-by: Iliya Savov <isavov@users.noreply.github.com>",
+          "timestamp": "2024-03-07T10:52:28Z",
+          "url": "https://github.com/isavov/madara/commit/98d433d4aac1a133d91237af68fc2365bba82984"
+        },
+        "date": 1709808956566,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Average TPS (Erc20 Transfers)",
+            "value": 65.83333333333333,
+            "unit": "transactions/second",
+            "extra": "CPU Count: 8\nCPU Model: \nCPU Speed (MHz): 0\nTotal Memory: 16 GB\nPlatform: MacOS 14.3\nRelease: 23.3.0\nArchitecture: aarch64"
+          },
+          {
+            "name": "Average Extrinsics per block (Erc20 Transfers)",
+            "value": 395,
+            "unit": "extrinsics/block",
+            "extra": "CPU Count: 8\nCPU Model: \nCPU Speed (MHz): 0\nTotal Memory: 16 GB\nPlatform: MacOS 14.3\nRelease: 23.3.0\nArchitecture: aarch64"
+          },
+          {
+            "name": "Average UOPS (Erc20 Transfers)",
+            "value": 65.83333333333333,
+            "unit": "operations/second",
+            "extra": "CPU Count: 8\nCPU Model: \nCPU Speed (MHz): 0\nTotal Memory: 16 GB\nPlatform: MacOS 14.3\nRelease: 23.3.0\nArchitecture: aarch64"
+          },
+          {
+            "name": "Average Steps Per Second (Erc20 Transfers)",
+            "value": 0,
+            "unit": "operations/second",
+            "extra": "CPU Count: 8\nCPU Model: \nCPU Speed (MHz): 0\nTotal Memory: 16 GB\nPlatform: MacOS 14.3\nRelease: 23.3.0\nArchitecture: aarch64"
+          },
+          {
+            "name": "Average TPS (Erc721 Mints)",
+            "value": 69.75,
+            "unit": "transactions/second",
+            "extra": "CPU Count: 8\nCPU Model: \nCPU Speed (MHz): 0\nTotal Memory: 16 GB\nPlatform: MacOS 14.3\nRelease: 23.3.0\nArchitecture: aarch64"
+          },
+          {
+            "name": "Average Extrinsics per block (Erc721 Mints)",
+            "value": 418.5,
+            "unit": "extrinsics/block",
+            "extra": "CPU Count: 8\nCPU Model: \nCPU Speed (MHz): 0\nTotal Memory: 16 GB\nPlatform: MacOS 14.3\nRelease: 23.3.0\nArchitecture: aarch64"
+          },
+          {
+            "name": "Average UOPS (Erc721 Mints)",
+            "value": 70.83333333333333,
+            "unit": "operations/second",
+            "extra": "CPU Count: 8\nCPU Model: \nCPU Speed (MHz): 0\nTotal Memory: 16 GB\nPlatform: MacOS 14.3\nRelease: 23.3.0\nArchitecture: aarch64"
+          },
+          {
+            "name": "Average Steps Per Second (Erc721 Mints)",
+            "value": 0,
+            "unit": "operations/second",
+            "extra": "CPU Count: 8\nCPU Model: \nCPU Speed (MHz): 0\nTotal Memory: 16 GB\nPlatform: MacOS 14.3\nRelease: 23.3.0\nArchitecture: aarch64"
+          }
+        ]
+      }
+    ]
+  }
+}
